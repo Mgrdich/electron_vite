bf3d387742bf7ca864105f24e2fe9b07827afe02
Mgrdich <mgotm13@gmail.com>
2023-05-08 22:52:02 +0400

    finalizing React project build

--- electron/main.cjs
+++ electron/main.cjs
@@ -26,7 +26,7 @@ function createWindow() {
         mainWindow.webContents.openDevTools();
     } else {
         // mainWindow.removeMenu();
-        mainWindow.loadFile(path.join(__dirname, 'build', 'index.html'));
+        mainWindow.loadFile(path.join(__dirname, '../build', 'index.html'));
     }
 }
 
